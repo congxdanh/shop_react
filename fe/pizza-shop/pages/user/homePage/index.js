eab5a1ea68ac55dec2d5999602de6d59b9e7d50f
congxdanh <danh.hsb13@gmail.com>
2024-01-18 20:48:30 +0700

    ss

---
+++ fe/pizza-shop/pages/user/homePage/index.js
@@ -0,0 +1,13 @@
+import { memo } from "react";
+
+const HomePages = () => {
+  return (
+    <>
+      <h1>Header</h1>
+      <h1>Body</h1>
+      <h1>Footer</h1>
+    </>
+  );
+};
+
+export default memo(HomePages);
